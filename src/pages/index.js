621702e735ec739bc84db40cb7aeafba8f7e7086
Muhammad Shafar <777uliahshafar@gmail.com>
2020-12-10 07:07:02 +0800

    testing to netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,18 +20,38 @@ const IndexPage = () => (
       <Image />
     </div>
     <Container>
-      <Form>
+      <Form
+        name="kuesioner awal"
+        method="post"
+        data-netlify="true"
+        onSubmit="submit"
+        data-netlify-honeypot="bot-field"
+      >
+        <p hidden>
+          <label>
+            I love honeypot : <input name="bot-field" />
+          </label>
+        </p>
+        <input type="hidden" name="form-name" value="kuesioner awal" />
         <Form.Group controlId="formBasicFullName">
           <Form.Label>Nama Lengkap</Form.Label>
-          <Form.Control type="text" placeholder="cth: Andi Akrab" />
+          <Form.Control
+            type="text"
+            name="nama-lengkap"
+            placeholder="cth: Andi Akrab"
+          />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Alamat Email</Form.Label>
-          <Form.Control type="text" placeholder="andiakrab@gmail.com" />
+          <Form.Control
+            type="text"
+            name="alamat-email"
+            placeholder="andiakrab@gmail.com"
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPhone">
           <Form.Label>Nomor HP (Optional)</Form.Label>
-          <Form.Control type="text" placeholder="08135519***" />
+          <Form.Control type="text" name="nomor-hp" placeholder="08135519***" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Kirim Kuesioner
